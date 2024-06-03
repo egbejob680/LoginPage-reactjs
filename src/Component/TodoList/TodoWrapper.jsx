3ba8ce2af5899fb7a558bdfbada910eec5b68515
egbejob680 <jobegbe39@gmail.com>
2024-06-03 13:10:23 +0100

    can now edit todo list

--- src/Component/TodoList/TodoWrapper.jsx
+++ src/Component/TodoList/TodoWrapper.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoForm } from '../TodoList/TodoForm';
 import { Todo } from './Todo';
+import { EditTodoForm } from './EditTodoForm';
 uuidv4();
 
 
@@ -15,7 +16,7 @@ export const TodoWrapper = () => {
       {
         id: uuidv4(),
         task: todo,
-        completed: false,
+        completed: true,
         isEditing: false,
       },
     ]);
@@ -33,21 +34,31 @@ export const TodoWrapper = () => {
 
   const handleEdit = (id) => {
     // Implement logic to set the todo with the given id to be in editing mode (isEditing: true)
+    setTodos(todos.map(todo => todo.id === id ? {...todo, isEditing: !todo.isEditing}: todo))
   };
 
+  const editTask = (task,id) => {
+    setTodos(todos.map(todo => todo.id ===id ?{...todo, task, isEditing: !todo.isEditing}: todo))
+  }
+
   return (
-    <div className='todo-wrapper w-1/2 display inline-block items-center my-8 mx-[500px] bg-white h-[50%] min-h-52 rounded-lg'>
+    <div className='todo-wrapper w-1/2 display overflow-hidden inline-block items-center my-8 mx-[500px] bg-white h-[50%] min-h-52 rounded-lg'>
       <h1 className='todo-title'>Get Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       
           {todos.map((todo,index) => (
-            <Todo
+            todo.isEditing ? (
+              <EditTodoForm editTodo={editTask} task={todo}/>
+            ) : (
+              <Todo
               key={index}
               task={todo}
               toggleComplete={toggleComplete}
               handleDelete={handleDelete}
               handleEdit={handleEdit} // Pass handleEdit function to Todo component for editing functionality
             />
+            )
+           
           ))}
        
     </div>
